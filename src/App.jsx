42cdf64bed2c8755565c775072ac4e31681bb1da
Chait <chaitknight81@gmail.com>
2026-02-20 02:19:22 -0500

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from './components/layout/Navigation/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navigation/Navigation.jsx
@@ -0,0 +1,45 @@
+/**
+ * Navigation Component
+ * Main site navigation bar with logo, links, and CTA button
+ */
+
+import './Navigation.css';
+
+const Navigation = () => {
+  // Logo
+  const logo = (
+    <img src="/logo.png" alt="CHAITcutsHair" className="navigation__logo" />
+  );
+
+  // Nav Links
+  const links = (
+    <ul className="navigation__links">
+      <li className="navigation__item">
+        <a href="#services" className="navigation__link">Services</a>
+      </li>
+      <li className="navigation__item">
+        <a href="#about" className="navigation__link">About</a>
+      </li>
+      <li className="navigation__item">
+        <a href="#contact" className="navigation__link">Contact</a>
+      </li>
+    </ul>
+  );
+
+  // CTA Button
+  const ctaButton = (
+    <a href="#" className="navigation__cta">Book Now</a>
+  );
+
+  return (
+    <nav className="navigation">
+      <div className="navigation__container">
+        {logo}
+        {links}
+        {ctaButton}
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
